0dd546088ac4d3b3fa41d2e572690361d1fe5427
Michael Ries <michael@riesd.com>
2014-06-06 16:56:49 -0600

    updating dependencies, etc

--- project.clj
+++ project.clj
@@ -1,15 +1,17 @@
-(defproject score_four "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
+(defproject score_four "0.1.0"
+  :description "A Simple Game of connect Four Dots"
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]]
+                 [org.clojure/clojurescript "0.0-2227"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [om "0.6.3"]]
 
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
 
   :source-paths ["src"]
 
-  :cljsbuild { 
+  :cljsbuild {
     :builds [{:id "score_four"
               :source-paths ["src"]
               :compiler {
