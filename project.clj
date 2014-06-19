294811a438b35d37890f4773006afcf0363fd1a7
Michael Ries <michael@riesd.com>
2014-06-18 23:05:39 -0600

    got some tests working

--- project.clj
+++ project.clj
@@ -5,17 +5,31 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2227"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
-                 [om "0.6.3"]]
+                 [om "0.6.3"]
+                 [com.cemerick/clojurescript.test "0.3.1"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
 
   :source-paths ["src"]
 
   :cljsbuild {
-    :builds [{:id "score_four"
+    :builds [
+             {:id "dev"
               :source-paths ["src"]
               :compiler {
                 :output-to "score_four.js"
                 :output-dir "out"
                 :optimizations :none
-                :source-map true}}]})
+                :source-map true}
+             }
+             {:id "unit-tests"
+              :source-paths ["src" "test"]
+              :compiler {
+                :output-to "score_four_test.js"
+                :optimizations :whitespace
+                :pretty-print true}
+             }
+            ]
+
+    :test-commands {
+        "unit-tests" ["phantomjs" "test_runner.js" "score_four_test.js"]}})
---
+++ test/score_four/board_test.cljs
@@ -0,0 +1,10 @@
+(ns score_four.board-test
+  (:require-macros [cemerick.cljs.test
+                    :refer (is deftest with-test run-tests testing test-var)])
+  (:require [cemerick.cljs.test :as t]))
+
+(deftest board-test
+  (is (= 2 2)))
+
+(deftest another
+  (is (= 1 2)))
