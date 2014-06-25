ab8bf930ce1e0810093f299f1568eb9d94571e53
Michael Ries <michael@riesd.com>
2014-06-25 11:29:57 -0600

    failing test for detecting a win

--- src/score_four/game.cljs
+++ src/score_four/game.cljs
@@ -1,12 +1,24 @@
 (ns score_four.game)
 
 (defn new-board []
-  [["white" "white" "white" "white" "white" "white" "white"]
-   ["white" "white" "white" "white" "white" "white" "white"]
-   ["white" "white" "white" "white" "white" "white" "white"]
-   ["white" "white" "white" "white" "white" "white" "white"]
-   ["white" "white" "white" "white" "white" "white" "white"]
-   ["white" "white" "white" "white" "white" "white" "white"]])
+  [["white" "white" "white" "white" "white" "white"]
+   ["white" "white" "white" "white" "white" "white"]
+   ["white" "white" "white" "white" "white" "white"]
+   ["white" "white" "white" "white" "white" "white"]
+   ["white" "white" "white" "white" "white" "white"]
+   ["white" "white" "white" "white" "white" "white"]
+   ["white" "white" "white" "white" "white" "white"]])
 
 (defn new-game []
-  {:build (new-board) :player "red" :winner nil})
+  {:board (new-board) :player "red" :winner nil})
+
+(defn- toggle-player [game]
+  (if (= (game :player) "red")
+    (assoc-in game '(:player) "black")
+    (assoc-in game '(:player) "red")))
+
+(defn- set-board-piece [game, column]
+  (assoc-in game (array :board column 5) (game :player)))
+
+(defn play [game column]
+  (-> game (set-board-piece column) (toggle-player)))
--- test/score_four/game_test.cljs
+++ test/score_four/game_test.cljs
@@ -14,3 +14,16 @@
 
 (deftest new-game-winner-nil
   (is (= (ng :winner) nil)))
+
+(deftest playing-an-open-board
+  (is (= (((g/play ng 0) :board) 0) ["white" "white" "white" "white" "white" "red"])))
+
+(deftest playing-multiple-turns
+  (let [gm (g/play (g/play ng 0) 1)]
+    (is (= ((gm :board) 0) ["white" "white" "white" "white" "white" "red"]))
+    (is (= ((gm :board) 1) ["white" "white" "white" "white" "white" "black"]))))
+
+(deftest play-until-win
+  (let [gm (-> ng (g/play 0) (g/play 1) (g/play 0) (g/play 1) (g/play 0) (g/play 1) (g/play 0))]
+    (is (= ((gm :board) 0) ["white" "white" "red" "red" "red" "red"]))
+    (is (= (gm :winner) "red"))))
